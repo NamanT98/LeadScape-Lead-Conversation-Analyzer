b11a7d097d1db74402d92a44f66dcbcc8b84a3fc
NamanT98 <namant@iitbhilai.ac.in>
2024-11-21 11:04:07 +0530

    added submit button

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from helper import *
 st.title("LeadScape")
 if "chatbot" not in st.session_state:
     st.session_state.chatbot = Chatbot()
+if "submit" not in st.session_state:
+    st.session_state.submit = False
 
 st.markdown("Paste your conversation here...")
 user_input = st.text_area(label="text area", height=400, label_visibility="hidden")
 
-if user_input:
+if st.button("Submit"):
+    st.session_state.submit = True
+
+if user_input and st.session_state.submit:
     response = st.session_state.chatbot.run_chain(user_input)
     st.markdown("Response")
     st.write("\n" + response)
